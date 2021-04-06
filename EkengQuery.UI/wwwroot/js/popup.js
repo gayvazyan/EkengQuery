91aa0558478ea6e434ec116a65a48393dfe8e270
garegin.ayvazyan <garegin.ayvazyan@outlook.com>
2021-04-06 12:24:40 +0400

    popup

---
+++ EkengQuery.UI/wwwroot/js/popup.js
@@ -0,0 +1,13 @@
+﻿function PopUp(hideOrshow) {
+    if (hideOrshow == 'hide') document.getElementById('wrapper').style.display = "none";
+    else document.getElementById('wrapper').removeAttribute('style');
+}
+
+window.onload = function () {
+    setTimeout(function () {
+        PopUp('show');
+        setTimeout(function () {
+            PopUp('hide');
+        }, 5000);
+    }, 1000);
+}
