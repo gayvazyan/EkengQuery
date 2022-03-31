6fcbb19a840b059d52edde5b0af78ea514babde2
garegin.ayvazyan <garegin.ayvazyan@outlook.com>
2022-03-31 09:29:50 +0400

    Null photo coretions

--- EkengQuery.UI/Pages/SsnQueryBPR.cshtml.cs
+++ EkengQuery.UI/Pages/SsnQueryBPR.cshtml.cs
@@ -19,7 +19,7 @@ namespace EkengQuery.UI.Pages
         public string Ssn { get; set; }
 
         [BindProperty]
-        public string ImageSource { get; set; }
+        public string ImageSource { get; set; } = "images/noPhoto.png";
 
         [BindProperty]
         public SSNWebServiceResponse Citizen { get; set; }
@@ -33,8 +33,13 @@ namespace EkengQuery.UI.Pages
             if (!String.IsNullOrEmpty(Ssn))
             {
                 Citizen = await _iBPRQuery.GetCitizenBySSN(Ssn);
-                byte[] data = Convert.FromBase64String(Citizen.Photo);
-                ImageSource = string.Format("data:image/png;base64,{0}", Convert.ToBase64String(data));
+                if (!String.IsNullOrEmpty(Citizen.Photo))
+                {
+                    byte[] data = Convert.FromBase64String(Citizen.Photo);
+                    ImageSource = string.Format("data:image/png;base64,{0}", Convert.ToBase64String(data));
+                }
+               
+               
             }
             return Page();
         }
